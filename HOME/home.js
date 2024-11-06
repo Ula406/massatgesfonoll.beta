b44bc210b65784618d6a778f71c503f74964233a
Oleguer Riera Fonoll <oleguerrierafonoll@gmail.com>
2024-11-06 15:05:52 +0100

    1.3.5

--- HOME/home.js
+++
@@ -1,6 +0,0 @@
-// home.js
-
-document.getElementById('logoutButton').addEventListener('click', function() {
-    localStorage.removeItem('isLoggedIn'); // Esborra el flag de sessió
-    window.location.href = 'login.html'; // Torna a la pàgina d'inici de sessió
-});
