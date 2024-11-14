3ecfdefb15fb3eda21ac2e829a92982632473a6f
Oleguer Riera Fonoll <oleguerrierafonoll@gmail.com>
2024-11-14 20:04:41 +0100

    1.4.2

--- login/validation.js
+++ login/validation.js
@@ -8,87 +8,73 @@ const error_message = document.getElementById('error-message')
 form.addEventListener('submit', (e) => {
   let errors = []
 
-  if (firstname_input) {
-    // Si tenim un camp de firstname, estem en el formulari de registre
+  if(firstname_input){
+    // If we have a firstname input then we are in the signup
     errors = getSignupFormErrors(firstname_input.value, email_input.value, password_input.value, repeat_password_input.value)
-  } else {
-    // Si no tenim un camp de firstname, estem en el formulari d'inici de sessió
+  }
+  else{
+    // If we don't have a firstname input then we are in the login
     errors = getLoginFormErrors(email_input.value, password_input.value)
   }
-
-  if (errors.length > 0) {
-    // Si hi ha errors, prevenim l'enviament del formulari i mostrem els errors
-    e.preventDefault()
-    error_message.innerText = errors.join(". ")
-  } else {
-    // Si no hi ha errors, creem la cookie amb les dades del formulari
-    const formData = {
-      email: email_input.value
-    }
-
-    if (firstname_input) {
-      formData.firstname = firstname_input.value  // Només si és un formulari de registre
-    }
-
-    // Codifiquem les dades del formulari a format JSON per emmagatzemar-les a la cookie
-    const formDataString = JSON.stringify(formData)
-
-    // Guardem les dades com una cookie (per exemple, durant 7 dies)
-    document.cookie = `formData=${encodeURIComponent(formDataString)}; path=/; max-age=${60*60*24*7};`
-
-    // Redirigim a la pàgina de "congratulations" després de guardar les dades
-    window.location.href = "https://ula406.github.io/massatgesfonoll.beta/congratulations.html"
+  e.preventDefault()
+  if(errors.length > 0){
+    // If there are any errors
+    error_message.innerText  = errors.join(". ")
   }
+  else{
+    document.cookie = `email=${email_input.value}`;
+    window.location.href = "https://ula406.github.io/massatgesfonoll.beta/congratulations.html" }
 })
 
-function getSignupFormErrors(firstname, email, password, repeatPassword) {
+function getSignupFormErrors(firstname, email, password, repeatPassword){
   let errors = []
 
-  if (firstname === '' || firstname == null) {
+  if(firstname === '' || firstname == null){
     errors.push('Firstname is required')
     firstname_input.parentElement.classList.add('incorrect')
   }
-  if (email === '' || email == null) {
+  if(email === '' || email == null){
     errors.push('Email is required')
     email_input.parentElement.classList.add('incorrect')
   }
-  if (password === '' || password == null) {
+  if(password === '' || password == null){
     errors.push('Password is required')
     password_input.parentElement.classList.add('incorrect')
   }
-  if (password.length < 8) {
+  if(password.length < 8){
     errors.push('Password must have at least 8 characters')
     password_input.parentElement.classList.add('incorrect')
   }
-  if (password !== repeatPassword) {
+  if(password !== repeatPassword){
     errors.push('Password does not match repeated password')
     password_input.parentElement.classList.add('incorrect')
     repeat_password_input.parentElement.classList.add('incorrect')
   }
 
-  return errors
+
+  return errors;
 }
 
-function getLoginFormErrors(email, password) {
+function getLoginFormErrors(email, password){
   let errors = []
 
-  if (email === '' || email == null) {
+  if(email === '' || email == null){
     errors.push('Email is required')
     email_input.parentElement.classList.add('incorrect')
   }
-  if (password === '' || password == null) {
+  if(password === '' || password == null){
     errors.push('Password is required')
     password_input.parentElement.classList.add('incorrect')
   }
 
-  return errors
+  return errors;
 }
 
 const allInputs = [firstname_input, email_input, password_input, repeat_password_input].filter(input => input != null)
 
 allInputs.forEach(input => {
   input.addEventListener('input', () => {
-    if (input.parentElement.classList.contains('incorrect')) {
+    if(input.parentElement.classList.contains('incorrect')){
       input.parentElement.classList.remove('incorrect')
       error_message.innerText = ''
     }
@@ -98,4 +84,4 @@ allInputs.forEach(input => {
 // Al fitxer validation.js o en una etiqueta <script>
 document.getElementById("back-button").addEventListener("click", function() {
   window.history.back(); // Redirigeix a la pàgina anterior
-})
+});
